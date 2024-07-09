13c2ed9c1fa12d61456ee5d04a5fb2df02eedb97
Privia <sudakalemontea@outlook.com>
2024-07-09 14:57:07 +0800

    Update exercise2_1.cpp

--- docs/Practise/chapter02/exercise2_1.cpp
+++ docs/Practise/chapter02/exercise2_1.cpp
@@ -1,12 +1,9 @@
-//
-// Created by Shujia Huang on 7/19/21.
-//
 #include <iostream>
 
 int main() {
 
     using namespace std;
 
-    cout << "Hi there, I'm Shujia Huang from Shenzhen, Guangdong, China" << endl;
+    cout << "Hi there, I'm Privia2023 from Yancheng, Jiangsu, China" << endl;
     return 0;
 }
