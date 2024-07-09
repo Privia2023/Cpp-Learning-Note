c2b9b2d4dc0c4091a7a7bb5646d2df03bb09582a
Privia <sudakalemontea@outlook.com>
2024-07-09 10:27:50 +0800

    main

---
+++ site/BookCodes/chapter02/carrots.cpp
@@ -0,0 +1,19 @@
+// food processing program
+
+#include <iostream>
+
+int main() {
+    using namespace std;
+
+    int carrots;
+
+    carrots = 25;
+    cout << "I have ";
+    cout << carrots;
+    cout << endl;
+
+    carrots = carrots - 1;
+    cout << "Crunch, crunch. Now I have " << carrots << " carrots." << endl;
+
+    return 0;
+}
